c8abe69e1240cb6fc925fd94aa5721c7347009ed
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-06-02 05:39:42 -0400

    Fix e2e and unit tests, and kondo issues. Limit scope of content translations to static embedding only.

--- e2e/support/helpers/e2e-dashboard-helpers.ts
+++ e2e/support/helpers/e2e-dashboard-helpers.ts
@@ -2,6 +2,8 @@ import type {
   DashCardId,
   DashboardCard,
   DashboardId,
+  DashboardTab,
+  VirtualDashboardCard,
   WritebackActionId,
 } from "metabase-types/api";
 
@@ -365,15 +367,6 @@ export function dashboardParametersPopover() {
   return popover({ testId: "parameter-value-dropdown" } as any);
 }
 
-/**
- * @param {Object} option
- * @param {number=} option.id
- * @param {number=} option.col
- * @param {number=} option.row
- * @param {number=} option.size_x
- * @param {number=} option.size_y
- * @param {string=} option.text
- */
 export function getTextCardDetails({
   id = getNextUnsavedDashboardCardId(),
   col = 0,
@@ -381,7 +374,8 @@ export function getTextCardDetails({
   size_x = 4,
   size_y = 6,
   text = "Text card",
-} = {}) {
+  ...cardDetails
+}: Partial<VirtualDashboardCard> = {}): Partial<VirtualDashboardCard> {
   return {
     id,
     card_id: null,
@@ -399,7 +393,20 @@ export function getTextCardDetails({
       },
       text,
     },
-  } as const;
+    ...cardDetails,
+  };
+}
+export function getDashboardTabDetails({
+  id,
+  name,
+}: Pick<DashboardTab, "id" | "name" | "position">): Pick<
+  DashboardTab,
+  "id" | "name" | "position"
+> {
+  return {
+    id,
+    name,
+  };
 }
 
 export function getHeadingCardDetails({
@@ -409,7 +416,8 @@ export function getHeadingCardDetails({
   size_x = 24,
   size_y = 1,
   text = "Heading text details",
-} = {}) {
+  ...cardDetails
+}: Partial<VirtualDashboardCard> = {}): Partial<VirtualDashboardCard> {
   return {
     id,
     card_id: null,
@@ -428,6 +436,7 @@ export function getHeadingCardDetails({
       "dashcard.background": false,
       text,
     },
+    ...cardDetails,
   };
 }
 
