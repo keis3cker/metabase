f5477b75b09b4798c9d5702af32289c31999e4d2
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-06-03 15:17:28 +0200

    Remove styled component for `UploadOverlay`
    [ci skip]

--- frontend/src/metabase/collections/components/UploadOverlay/UploadOverlay.tsx
+++ frontend/src/metabase/collections/components/UploadOverlay/UploadOverlay.tsx
@@ -1,9 +1,9 @@
 import { t } from "ttag";
 
-import { Icon } from "metabase/ui";
+import { Center, Icon, Stack } from "metabase/ui";
 import type { Collection } from "metabase-types/api";
 
-import { DragOverlay } from "./UploadOverlay.styled";
+import S from "./UploadOverlay.module.css";
 
 export function UploadOverlay({
   isDragActive,
@@ -13,9 +13,23 @@ export function UploadOverlay({
   collection: Collection;
 }) {
   return (
-    <DragOverlay isDragActive={isDragActive}>
-      <Icon name="upload" size="24" />
-      <div>{t`Drop here to upload to ${collection.name}`}</div>
-    </DragOverlay>
+    <Center
+      className={S.dragOverlay}
+      pos="absolute"
+      top={0}
+      right={0}
+      bottom={0}
+      left={0}
+      bd="1px dashed var(--mb-color-brand)"
+      bg="brand-lighter"
+      m="0.5rem 4%"
+      opacity={isDragActive ? 0.9 : 0}
+      p="4rem"
+    >
+      <Stack gap="md" c="brand" align="center" ta="center" fz="lg" fw={700}>
+        <Icon name="upload" size="24" />
+        <div>{t`Drop here to upload to ${collection.name}`}</div>
+      </Stack>
+    </Center>
   );
 }
